d228904b738928c84c7f078d8b81bb28e60bf347
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-30 11:08:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// write code here
+function checkPermission(role, admin, notAdmin){
+  if(role == `ADMIN`){
+    isAdminAction()
+  }else{
+    isNotAdminAction()
+  }
+}
+  function isAdminAction(){
+    alert(`Access granted`)
+  }
+  function isNotAdminAction(){
+    alert(`Access denied`)
+  }
+checkPermission("ADMIN", isAdminAction, isNotAdminAction);
+checkPermission("USER", isAdminAction, isNotAdminAction);
